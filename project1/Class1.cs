686081e0680c11b18eefe1a3a086442d48300373
koplu <onlinetraining99831@gmail.com>
2019-01-23 21:52:04 +0530

    initial commit

---
+++ project1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    public class Class1
+    {
+        public void test()
+        {
+            Console.WriteLine("test");
+            Console.WriteLine("test");
+        }
+    }
+}
